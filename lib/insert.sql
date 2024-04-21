5146710cd0e5b86d10d78dc7dd34e09cc7583e09
PapiLuee <41023873+LueDev@users.noreply.github.com>
2024-04-21 18:23:38 -0400

    CRUD in SQL is a cakewalk 8)

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Mr. Chocolate', 7, 'M', 'BROWN', 'Chill', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Rowdy', 8, 'F', 'BROWN', 'Happy', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Tabitha', 9, 'F', 'BROWN', 'Randy', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Sergeant Brown', 10, 'M', 'BROWN', 'Combative', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Melissa', 4, 'F', 'BLUE', 'Weird', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Grinch', 7, 'M', 'GREEN', 'Mischiveous', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Wendy', 3, 'F', 'BROWN', 'Curious', 1);
+INSERT INTO bears(age, sex, color, temperament, alive) VALUES(3, 'F', 'BROWN', 'Curious', 1);
